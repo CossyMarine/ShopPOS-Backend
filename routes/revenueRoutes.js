347c1d5c79e5c81b0dd25b713ae3bc49b426dab6
CossyMarine <shidayawatukibwari@gmail.com>
2026-08-01 00:38:23 +0300

    Update revenueRoutes.js

--- routes/revenueRoutes.js
+++ routes/revenueRoutes.js
@@ -1,10 +1,12 @@
 // routes/revenueRoutes.js
 import express from "express";
-import { getTodayRevenue, getRevenueSummary } from "../controllers/revenueController.js";
+import { getTodayRevenue, getRevenueSummary, getDashboardStats } from "../controllers/revenueController.js";
 import { protect, authorize } from "../Middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/today", getTodayRevenue);
 router.get("/summary", protect, authorize("admin", "branchManager"), getRevenueSummary);
+router.get("/dashboard-stats", protect, authorize("admin", "branchManager"), getDashboardStats);
+
 export default router;
