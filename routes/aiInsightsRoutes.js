4875ebfefa82ecba1b3050a5afbdc34fce69d2d2
CossyMarine <shidayawatukibwari@gmail.com>
2026-08-04 22:22:42 +0300

    Update aiInsightsRoutes.js

--- routes/aiInsightsRoutes.js
+++ routes/aiInsightsRoutes.js
@@ -1,10 +1,11 @@
 // routes/aiInsightsRoutes.js
 import express from "express";
-import { runStoreAudit } from "../controllers/aiInsightsController.js";
+import { runStoreAudit, listGeminiModels } from "../controllers/aiInsightsController.js";
 import { protect, authorize } from "../Middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/audit", protect, authorize("admin", "branchManager"), runStoreAudit);
+router.get("/list-models", protect, authorize("admin", "branchManager"), listGeminiModels);
 
 export default router;
