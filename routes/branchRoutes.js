47ee72b5b9ef8e51a8e73e0e23d887e7bbceb5a5
CossyMarine <shidayawatukibwari@gmail.com>
2026-08-04 10:38:38 +0300

    Update branchRoutes.js

--- routes/branchRoutes.js
+++ routes/branchRoutes.js
@@ -1,11 +1,12 @@
 // routes/branchRoutes.js
 import express from "express";
-import { getBranches, createBranch, updateBranch, assignManager, getAllStaff } from "../controllers/branchController.js";
+import { getBranches, createBranch, updateBranch, assignManager, getAllStaff, getMyBranch } from "../controllers/branchController.js";
 import { protect, authorize } from "../Middlewares/authMiddleware.js";
 
 const router = express.Router();
 const adminOnly = authorize("admin");
 
+router.get("/mine", protect, getMyBranch);
 router.get("/", protect, adminOnly, getBranches);
 router.get("/staff", protect, adminOnly, getAllStaff);
 router.post("/", protect, adminOnly, createBranch);
