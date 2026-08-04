e2e2dfe875d449842f76ed07a6e0f10ea17bd936
CossyMarine <shidayawatukibwari@gmail.com>
2026-08-04 14:30:26 +0300

    Update wageRoutes.js

--- routes/wageRoutes.js
+++ routes/wageRoutes.js
@@ -1,13 +1,19 @@
 // routes/wageRoutes.js
 import express from "express";
 import { protect, authorize } from "../Middlewares/authMiddleware.js";
-import { getWageProfile, upsertWageProfile, listWageProfiles } from "../controllers/wageController.js";
+import {
+  getWageProfile,
+  upsertWageProfile,
+  listWageProfiles,
+  getDeductionOptions, // NEW
+} from "../controllers/wageController.js";
 
 const router = express.Router();
 router.use(protect, authorize("admin", "branchManager"));
 
 router.get("/", listWageProfiles);
 router.get("/:userId", getWageProfile);
+router.get("/:userId/deduction-options", getDeductionOptions); // NEW
 router.put("/:userId", upsertWageProfile);
 
 export default router;
