8d2b81a17e7c176ddd89e62ec46805edf5fd53c1
CossyMarine <shidayawatukibwari@gmail.com>
2026-08-25 02:15:34 +0300

    Create requestId.js

---
+++ Middlewares/requestId.js
@@ -0,0 +1,8 @@
+// Middlewares/requestId.js
+import { v4 as uuidv4 } from "uuid";
+
+export const requestId = (req, res, next) => {
+  req.requestId = req.headers["x-request-id"] || uuidv4();
+  res.setHeader("x-request-id", req.requestId);
+  next();
+};
