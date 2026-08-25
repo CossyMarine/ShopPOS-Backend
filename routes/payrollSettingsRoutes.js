aa69a524ab898edcb45fc763313ed3b7dd07d3c7
CossyMarine <shidayawatukibwari@gmail.com>
2026-08-25 13:55:05 +0300

    Update payrollSettingsRoutes.js

--- routes/payrollSettingsRoutes.js
+++ routes/payrollSettingsRoutes.js
@@ -2,10 +2,12 @@
 import express from "express";
 import { protect, authorize } from "../Middlewares/authMiddleware.js";
 import { getPayrollSettings, upsertPayrollSettings } from "../controllers/payrollSettingsController.js";
+import { validateUpsertPayrollSettings } from "../Middlewares/validators/payrollSettingsValidators.js";
+import { validate } from "../Middlewares/validate.js";
 
 const router = express.Router();
 router.use(protect, authorize("admin", "branchManager"));
 router.get("/", getPayrollSettings);
-router.put("/", upsertPayrollSettings);
+router.put("/", validateUpsertPayrollSettings, validate, upsertPayrollSettings);
 
 export default router;
