ec00258ee01f97abdc4f674de84cd59e77ef942d
CossyMarine <shidayawatukibwari@gmail.com>
2026-08-25 13:40:37 +0300

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,10 +2,20 @@
 import express from "express";
 import { createOrder } from "../controllers/orderController.js";
 import { protect, authorize, requireOpenShift } from "../Middlewares/authMiddleware.js";
+import { validateCreateOrder } from "../Middlewares/validators/orderValidators.js";
+import { validate } from "../Middlewares/validate.js";
 
 const router = express.Router();
 
 // Finalizes a checkout: creates the sale, deducts stock FIFO, generates the receipt
-router.post("/", protect, authorize("cashier", "branchManager", "admin"), requireOpenShift, createOrder);
+router.post(
+  "/",
+  protect,
+  authorize("cashier", "branchManager", "admin"),
+  requireOpenShift,
+  validateCreateOrder,
+  validate,
+  createOrder
+);
 
 export default router;
